aebc06790df652541f47c70d5ea19b8ede225c67
João Pedro Patrício do Nascimento <joaoppn00@gmail.com>
2022-10-14 18:51:59 -0300

    Versão 0.3

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import datetime
+from select import select
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
@@ -6,18 +8,30 @@ load_dotenv()
 def create_app():
     app=Flask(__name__)
 
-
+    @app.context_processor
+    def add_calc_date_range():
+        def date_range(start: datetime.date):
+            dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+            return dates
+        return {"date_range": date_range}
+    
     habits = []
 
     @app.route("/")
     def day():
-        return render_template("day.html", habits=habits, title="Day a Day")
+        date_str = request.args.get("date")
+        if date_str:
+            selected_date = datetime.date.fromisoformat(date_str)
+        else:
+            selected_date = datetime.date.today()
+        
+        return render_template("day.html", habits=habits, title="Day a Day", selected_date=selected_date)
 
 
     @app.route("/add", methods=["GET", "POST"])
     def add_day():
         if request.method == "POST":
             habits.append(request.form.get("habit"))
-        return render_template("add_day.html", title="Day a day - Add")
+        return render_template("add_day.html", title="Day a day - Add", selected_date=datetime.date.today())
 
     return app
