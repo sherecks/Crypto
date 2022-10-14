284472e9da933dc0890248b65fd6f231ff2b7836
João Pedro Patrício do Nascimento <joaoppn00@gmail.com>
2022-10-14 12:25:40 -0300

    Versão 0.2

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ def create_app():
     app=Flask(__name__)
 
 
-    habits = ["Test Habit", "Test Habit 2"]
+    habits = []
 
     @app.route("/")
     def day():
-        return render_template("day.html", title="Day a Day")
+        return render_template("day.html", habits=habits, title="Day a Day")
 
 
     @app.route("/add", methods=["GET", "POST"])
     def add_day():
-        if request.form == "POST":
+        if request.method == "POST":
             habits.append(request.form.get("habit"))
         return render_template("add_day.html", title="Day a day - Add")
 
