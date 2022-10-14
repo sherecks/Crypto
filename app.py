9d257d73c7c8c165ac96ac08c5d3c1027b34c758
João Pedro Patrício do Nascimento <joaoppn00@gmail.com>
2022-10-14 20:30:09 -0300

    Versão 0.4

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import datetime
 from select import select
-from flask import Flask, render_template, request
+from collections import defaultdict
+from flask import Flask, render_template, request, url_for, redirect
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def create_app():
     app=Flask(__name__)
+    completions = defaultdict(list) 
+
+
 
     @app.context_processor
     def add_calc_date_range():
@@ -25,7 +29,7 @@ def create_app():
         else:
             selected_date = datetime.date.today()
         
-        return render_template("day.html", habits=habits, title="Day a Day", selected_date=selected_date)
+        return render_template("day.html", habits=habits, title="Day a Day", selected_date=selected_date, completions=completions[selected_date])
 
 
     @app.route("/add", methods=["GET", "POST"])
@@ -33,5 +37,15 @@ def create_app():
         if request.method == "POST":
             habits.append(request.form.get("habit"))
         return render_template("add_day.html", title="Day a day - Add", selected_date=datetime.date.today())
+
+    @app.route("/complete", methods=["POST"])
+    def complete():
+        date_string = request.form.get("date")
+        habit = request.form.get("habitname")
+        date = datetime.date.fromisoformat(date_string)
+        completions[date].append(habit)
+
+        return redirect(url_for("day", date=date_string))
+
 
     return app
